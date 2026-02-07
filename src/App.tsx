409f0fdfe930ca6e7028d880f4770ea81e2fd62b
janjaw007 <chayanit.kitm@gmail.com>
2026-02-07 14:23:20 +0700

    added:shadcnUI and react-router and init Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Products from "./pages/Products/Products";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
-  return <h1>hello-world</h1>;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="products" element={<Products />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
