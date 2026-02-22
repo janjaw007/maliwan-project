97d4a2af258c36f93543ae9c02f35ebfe42e664b
janjaw007 <chayanit.kitm@gmail.com>
2026-02-22 23:07:43 +0700

    add:navbar mobile menu layout

--- src/components/Navbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar.tsx
@@ -1,25 +1,47 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 function MobileNavbar() {
   const [openNav, setOpenNav] = useState(false);
 
   return (
     <>
-      <div className="bg-red-300 w-full h-full flex justify-between">
-        <div className="flex flex-col ">
-          <span>Maliwan</span>
-          <span>Alumium & Glass</span>
-        </div>
-        <div>
-          <button onClick={() => setOpenNav((currentState) => !currentState)}>
-            burger logo
+      <div className="w-full h-full flex justify-between px-3 border-b border-default">
+        <Link to={"/"} onClick={() => setOpenNav(false)}>
+          <div className="flex flex-col ">
+            <span className="font-mono font-bold uppercase">Maliwan</span>
+            <span className="font-mono tracking-widest">Alumium & Glass</span>
+          </div>
+        </Link>
+        <div className="flex items-center">
+          <button
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary cursor-pointer"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+            onClick={() => setOpenNav((currentState) => !currentState)}
+          >
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="M5 7h14M5 12h14M5 17h14"
+              />
+            </svg>
           </button>
         </div>
       </div>
       {openNav && (
         <>
-          <ul className="w-full ">
+          <ul className="w-full px-3 py-1 bg-amber-100 flex flex-col justify-center">
             <OpenNavItem to={"/"} label="Home" setOpenNav={setOpenNav} />
             <OpenNavItem to={"about"} label="About" setOpenNav={setOpenNav} />
             <OpenNavItem
@@ -54,10 +76,16 @@ interface OpenNavItemProps {
 
 function OpenNavItem({ to, label, setOpenNav }: OpenNavItemProps) {
   return (
-    <li className="flex items-center h-10 bg-amber-200">
-      <Link onClick={() => setOpenNav(false)} to={to}>
-        {label}
-      </Link>
-    </li>
+    <NavLink
+      className={({ isActive }) =>
+        isActive
+          ? "flex items-center h-10 rounded-lg w-full px-4  font-semibold font-mono capitalize bg-amber-700 text-white"
+          : "flex items-center h-10 rounded-lg w-full px-4  font-semibold font-mono capitalize hover:bg-amber-200 cursor-pointer"
+      }
+      onClick={() => setOpenNav(false)}
+      to={to}
+    >
+      <li>{label}</li>
+    </NavLink>
   );
 }
